261bf5092275f9d08921fbabf23d5f42ac9e2501
MMA <>
2023-11-14 18:54:43 +0100

    add verbose mode

--- check_file-stringcount.sh
+++ check_file-stringcount.sh
@@ -6,15 +6,16 @@
 # Check the stringcount in a file. WARNING -w - CRITICAL -c 
 # Example: ./check_file-stringcount.sh -w 10 -c 20
 
-while getopts :f:s:w:c: options
+while getopts :f:s:w:c:v options
 do
   case $options in
        f) FILE=${OPTARG};;
        s) STRING=${OPTARG};;
        w) WARN=${OPTARG};;
        c) CRIT=${OPTARG};;
-       :) echo "YOU HAVE TO USE -f FILE -s STRING -w WARNING and -c CRITICAL" && exit 1;;
-       *) echo "Parameter unknown. YOU HAVE TO USE -f FILE -s STRING -w WARNING and -c CRITICAL" && exit 1;;
+       v) VERBOSE="ON";;
+       :) echo "YOU HAVE TO USE -f FILE -s STRING -w WARNING and -c CRITICAL. Parameter -v for verbose" && exit 1;;
+       *) echo "Parameter unknown. YOU HAVE TO USE -f FILE -s STRING -w WARNING and -c CRITICAL. Parameter -v for verbose" && exit 1;;
   esac
 done
 
@@ -23,14 +24,26 @@ STRINGCOUNT=$(cat ${FILE} | grep ${STRING} | wc -l)
 if [ ${STRINGCOUNT} -lt ${WARN} ]
 then
   echo "STRINGCOUNT IS OK = ${STRINGCOUNT}"
+  if [ "$VERBOSE" == "ON" ]
+  then
+    cat ${FILE} | grep ${STRING}
+  fi
   exit 0
 elif [ ${STRINGCOUNT} -ge ${WARN} ] && [ ${STRINGCOUNT} -lt ${CRIT} ]
 then
   echo "STRINGCOUNT IS IN WARNING = ${STRINGCOUNT}"
+  if [ "$VERBOSE" == "ON" ]
+  then
+    cat ${FILE} | grep ${STRING}
+  fi
   exit 1
 elif [ ${STRINGCOUNT} -ge ${CRIT} ]
 then
   echo "STRINGCOUNT IS IN CRITICAL = ${STRINGCOUNT}"
+  if [ "$VERBOSE" == "ON" ]
+  then
+    cat ${FILE} | grep ${STRING}
+  fi
   exit 2
 else
   echo "STRINGCOUNT IS IN UNKOWN"
